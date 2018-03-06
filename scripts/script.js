2a5030b896e40a39529e83333db2841e3a06e377
Artem Anufrij <artem.anufrij@live.de>
2018-03-06 21:35:08 +0100

    donate

--- scripts/script.js
+++ scripts/script.js
@@ -2,4 +2,7 @@ $(document).ready(function () {
     $.get("../footer.html", function (data) {
         $("footer").html(data);
     });
+    $.get("../donate.html", function (data) {
+        $("donate").html(data);
+    });
 });
